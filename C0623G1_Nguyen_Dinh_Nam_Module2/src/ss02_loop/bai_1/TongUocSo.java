32a737988faf115a93b711d5bbc1e1328e5b7482
dinhnam72 <dinhnam20162011@gmail.com>
2023-08-10 01:11:08 +0700

    nộp bài tap ss2

--- C0623G1_Nguyen_Dinh_Nam_Module2/src/ss02_loop/bai_1/TongUocSo.java
+++ C0623G1_Nguyen_Dinh_Nam_Module2/src/ss02_loop/bai_1/TongUocSo.java
@@ -1,2 +1,19 @@
-package ss02_loop.bai_1;public class TongUocSo {
+package ss02_loop.bai_1;
+
+import java.util.Scanner;
+
+public class TongUocSo {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("So muon tim uoc: ");
+        int a = Integer.parseInt(scanner.nextLine());
+        int sum = 0;
+        for (int i = 1; i <= a; i++) {
+            if (a % i == 0) {
+                sum += i;
+                System.out.println("Uoc cua " + a + ": " + i);
+            }
+        }
+        System.out.println("Tong uoc so cua " + a + ": " + sum);
+    }
 }
--- C0623G1_Nguyen_Dinh_Nam_Module2/src/ss02_loop/bai_2/TrungBinhCong.java
+++ C0623G1_Nguyen_Dinh_Nam_Module2/src/ss02_loop/bai_2/TrungBinhCong.java
@@ -1,2 +1,31 @@
-package ss02_loop.bai_2;public class TrungBinhCong {
+package ss02_loop.bai_2;
+
+import java.util.Scanner;
+
+public class TrungBinhCong {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("So thu nhat: ");
+        int a = Integer.parseInt(scanner.nextLine());
+        System.out.print("So thu hai: ");
+        int b = Integer.parseInt(scanner.nextLine());
+        System.out.println("USCLN của " + a + " và " + b
+                + " là: " + uSCLN(a, b));
+        System.out.println("BSCNN của " + a + " và " + b
+                + " là: " + bSCNN(a, b));
+        System.out.println("Trung bình cộng: " + tBC(uSCLN(a, b), bSCNN(a, b)));
+    }
+
+    public static int uSCLN(int a, int b) {
+        if (b == 0) return a;
+        return uSCLN(b, a % b);
+    }
+
+    public static int bSCNN(int a, int b) {
+        return (a * b) / uSCLN(a, b);
+    }
+
+    public static double tBC(int uSCLN, int bSCLN) {
+        return (uSCLN + bSCLN) / 2.0;
+    }
 }
