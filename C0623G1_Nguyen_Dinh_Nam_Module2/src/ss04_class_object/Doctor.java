6d1112640e0abdc38debbc2264ebb9f4e37cb41c
dinhnam72 <dinhnam20162011@gmail.com>
2023-08-10 16:11:45 +0700

    nộp bài tap ss4

--- C0623G1_Nguyen_Dinh_Nam_Module2/src/ss04_class_object/Doctor.java
+++ C0623G1_Nguyen_Dinh_Nam_Module2/src/ss04_class_object/Doctor.java
@@ -1,2 +1,28 @@
-package ss04_class_object;public class Doctor {
+package ss04_class_object;
+
+public class Doctor {
+    private String name;
+    private int age;
+    private String addess;
+    private String email;
+    private String location;
+
+    public Doctor(String name, int age, String addess, String email, String location) {
+        this.name = name;
+        this.age = age;
+        this.addess = addess;
+        this.email = email;
+        this.location = location;
+    }
+
+    @Override
+    public String toString() {
+        return "Doctor{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                ", addess='" + addess + '\'' +
+                ", email='" + email + '\'' +
+                ", location='" + location + '\'' +
+                '}';
+    }
 }
--- C0623G1_Nguyen_Dinh_Nam_Module2/src/ss04_class_object/Test.java
+++ C0623G1_Nguyen_Dinh_Nam_Module2/src/ss04_class_object/Test.java
@@ -1,2 +1,13 @@
-package ss04_class_object;public class Test {
+package ss04_class_object;
+
+public class Test {
+    public static void main(String[] args) {
+        Doctor doctor1 = new Doctor("Nguyen Van A", 24,"Quang Binh","affgr","Hoc Sinh");
+        Doctor doctor2 = new Doctor("Tran Thi B", 20,"Ha Noi","affgr","Giam Doc");
+        Doctor doctor3 = new Doctor("Hoang Hai", 15,"Quang Binh","affgr","Hoc Sinh");
+       Doctor[] doctorList = {doctor1,doctor2,doctor3};
+       for (Doctor doctor:doctorList){
+           System.out.println(doctor);
+       }
+    }
 }
