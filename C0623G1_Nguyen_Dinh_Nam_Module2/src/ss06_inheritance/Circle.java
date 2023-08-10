ef00f1185fecc27eccfec24573b8c1c7500853ea
dinhnam72 <dinhnam20162011@gmail.com>
2023-08-10 16:12:30 +0700

    nộp bài tap ss6

--- C0623G1_Nguyen_Dinh_Nam_Module2/src/ss06_inheritance/Circle.java
+++ C0623G1_Nguyen_Dinh_Nam_Module2/src/ss06_inheritance/Circle.java
@@ -1,2 +1,39 @@
-package ss06_inheritance;public class Circle {
+package ss06_inheritance;
+
+public class Circle {
+    // thanh phan duoc ke thua
+    private double radius;
+    // thanh phan duoc ke thua
+    private String color;
+
+    public Circle() {
+    }
+    // overload
+    public Circle(double radius, String color) {
+        this.radius = radius;
+        this.color = color;
+    }
+
+    public double getRadius() {
+        return radius;
+    }
+
+    public void setRadius(double radius) {
+        this.radius = radius;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    @Override
+    public String toString() {
+        return "A Circle with radius = "
+                + this.radius
+                + " And color is " +this.color;
+    }
 }
--- C0623G1_Nguyen_Dinh_Nam_Module2/src/ss06_inheritance/Cylinder.java
+++ C0623G1_Nguyen_Dinh_Nam_Module2/src/ss06_inheritance/Cylinder.java
@@ -1,2 +1,30 @@
-package ss06_inheritance;public class Cylinder {
+package ss06_inheritance;
+
+public class Cylinder extends Circle{
+    // thanh phan khong duoc ke thua
+    private double height;
+
+    public Cylinder() {
+    }
+    // overload
+    public Cylinder(double radius, String color, double height) {
+        super(radius, color);
+        this.height = height;
+    }
+
+    public double getHeight() {
+        return height;
+    }
+
+    public void setHeight(double height) {
+        this.height = height;
+    }
+    // override cua circle
+    @Override
+    public String toString() {
+        return "A Cylinder with height is "
+                + this.height
+                + " , which is a subclass of "
+                + super.toString();
+    }
 }
