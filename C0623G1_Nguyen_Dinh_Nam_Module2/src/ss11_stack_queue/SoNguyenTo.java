b45a1279ba2251e9a71a146d0d3ef33405c834a1
dinhnam72 <dinhnam20162011@gmail.com>
2023-08-11 15:26:23 +0700

    nop bai tap ss11

--- C0623G1_Nguyen_Dinh_Nam_Module2/src/ss11_stack_queue/SoNguyenTo.java
+++ C0623G1_Nguyen_Dinh_Nam_Module2/src/ss11_stack_queue/SoNguyenTo.java
@@ -1,2 +1,46 @@
-package ss11_stack_queue;public class SoNguyenTo {
+package ss11_stack_queue;
+
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Stack;
+
+public class SoNguyenTo {
+    public boolean ktSNT(int n) {
+        int count = 0;
+        for (int i = 1; i <= n / 2; i++) {
+            if (n % i == 0) {
+                count++;
+            }
+        }
+        if (count == 1) {
+            return true;
+        }
+        return false;
+    }
+
+    public void sntToStack(int n) {
+        Stack<Integer> stack = new Stack<>();
+        for (int i = 0; i < n; i++) {
+            if (ktSNT(i)) {
+                stack.push(i);
+            }
+        }
+        System.out.print("Phan tu trong stack: ");
+        while (!stack.isEmpty()) {
+            System.out.print(stack.pop() + " ");
+        }
+    }
+    public void sntToQueue(int n){
+        Queue<Integer> queue = new LinkedList<>();
+        for (int i = 0; i < n; i++) {
+            if (ktSNT(i)) {
+               queue.add(i);
+            }
+        }
+        System.out.println();
+        System.out.print("Phan tu trong queue: ");
+        while (!queue.isEmpty()) {
+            System.out.print(queue.poll() + " ");
+        }
+    }
 }
--- C0623G1_Nguyen_Dinh_Nam_Module2/src/ss11_stack_queue/Test.java
+++ C0623G1_Nguyen_Dinh_Nam_Module2/src/ss11_stack_queue/Test.java
@@ -1,2 +1,15 @@
-package ss11_stack_queue;public class Test {
+package ss11_stack_queue;
+
+import ss08_clean_code.Empluyee;
+
+import java.util.List;
+import java.util.Stack;
+
+public class Test {
+    public static void main(String[] args) {
+        SoNguyenTo snt = new SoNguyenTo();
+        int n = 25;
+        snt.sntToStack(n);
+        snt.sntToQueue(n);
+    }
 }
