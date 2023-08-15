f6f44200769e612cba1076e7311f16b73f4e60ce
dinhnam72 <dinhnam20162011@gmail.com>
2023-08-15 15:52:46 +0700

    nop bai tap ss19

---
+++ C0623G1_Nguyen_Dinh_Nam_Module2/src/ss19_string_regex/ValidateEmail.java
@@ -0,0 +1,26 @@
+package ss19_string_regex;
+
+public class ValidateEmail {
+    public static void main(String[] args) {
+        String test1 = "asdasdsa@sdfds.sdfsdf";
+        String test2 = "Asdas3432dsa@sdfds.sdfsdf";
+        String test3 = "asAD343dasdsa@gmail.c";
+        System.out.print(test1 + " : ");
+        checkString(test1);
+        System.out.print(test2 + " : ");
+        checkString(test2);
+        System.out.print(test3 + " : ");
+        checkString(test3);
+
+    }
+
+    private static final String REX = "^\\w{9,32}@\\w{2,12}\\.\\w{2,12}";
+
+    public static void checkString(String str) {
+        if (str.matches(REX)) {
+            System.out.println("Chuoi hop le");
+        } else {
+            System.out.println("Chuoi khong hop le");
+        }
+    }
+}
