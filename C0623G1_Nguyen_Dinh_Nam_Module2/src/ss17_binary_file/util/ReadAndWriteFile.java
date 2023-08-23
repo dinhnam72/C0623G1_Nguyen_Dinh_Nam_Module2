e60d3050b962dcc7f3998be8c00b956787faf9db
dinhnam72 <dinhnam20162011@gmail.com>
2023-08-23 15:25:31 +0700

    nop bai tap ss17

--- C0623G1_Nguyen_Dinh_Nam_Module2/src/ss17_binary_file/util/ReadAndWriteFile.java
+++ C0623G1_Nguyen_Dinh_Nam_Module2/src/ss17_binary_file/util/ReadAndWriteFile.java
@@ -9,10 +9,11 @@ import java.util.List;
 public class ReadAndWriteFile {
     public static List<Work> readFile(String path) {
         List<Work> workList = new ArrayList<>();
-        try (
-                FileInputStream fis = new FileInputStream(path);
-                ObjectInputStream ois = new ObjectInputStream(fis)
-        ) {
+        FileInputStream fis = null;
+        ObjectInputStream ois = null;
+        try {
+            fis = new FileInputStream(path);
+            ois = new ObjectInputStream(fis);
 
             workList = (List<Work>) ois.readObject();
         } catch (FileNotFoundException e) {
@@ -21,22 +22,45 @@ public class ReadAndWriteFile {
             System.out.println("Error content!");
         } catch (ClassNotFoundException e) {
             System.out.println("Class not found!");
+        } finally {
+            try {
+                if (ois != null) {
+                    ois.close();
+                }
+                if (fis != null) {
+                    fis.close();
+                }
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
         }
         return workList;
     }
-    public static void writeFile(String path, List<Work> workList) {
-        try (
-                FileOutputStream fos = new FileOutputStream(path);
-                ObjectOutputStream oos = new ObjectOutputStream(fos)
-        ) {
 
+    public static void writeFile(String path, List<Work> workList) {
+        FileOutputStream fos = null;
+        ObjectOutputStream oos = null;
+        try {
+            fos = new FileOutputStream(path);
+            oos = new ObjectOutputStream(fos);
             oos.writeObject(workList);
 
         } catch (FileNotFoundException e) {
             System.out.println("Path not found!");
         } catch (IOException e) {
             System.out.println("Error content!");
-        }
+        } finally {
+            try {
+                if (oos != null) {
+                    oos.close();
+                }
+                if (fos != null) {
+                    fos.close();
+                }
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
 
+        }
     }
 }
