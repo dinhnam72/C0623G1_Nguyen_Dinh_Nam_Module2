474428e287d910fc68768acc068ab31ec99a5d89
dinhnam72 <dinhnam20162011@gmail.com>
2023-08-23 15:25:53 +0700

    nop bai tap case study

--- C0623G1_Nguyen_Dinh_Nam_Module2/src/case_study/util/RAWFile.java
+++ C0623G1_Nguyen_Dinh_Nam_Module2/src/case_study/util/RAWFile.java
@@ -5,34 +5,62 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class RAWFile {
-    public static List<String> readToCSV(String filePath){
+    public static List<String> readToCSV(String filePath) {
         List<String> stringList = new ArrayList<>();
         File file = new File(filePath);
-        try (FileReader fileReader = new FileReader(file);
-             BufferedReader bufferedReader = new BufferedReader(fileReader)
-        ){
+        FileReader fileReader = null;
+        BufferedReader bufferedReader = null;
+        try {
+            fileReader = new FileReader(file);
+            bufferedReader = new BufferedReader(fileReader);
             String line;
-            while ((line=bufferedReader.readLine())!=null){
+            while ((line = bufferedReader.readLine()) != null) {
                 stringList.add(line);
             }
         } catch (FileNotFoundException e) {
             throw new RuntimeException(e);
         } catch (IOException e) {
             throw new RuntimeException(e);
+        } finally {
+            try {
+                if (bufferedReader != null) {
+                    bufferedReader.close();
+                }
+                if (fileReader != null) {
+                    fileReader.close();
+                }
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
         }
         return stringList;
     }
-    public static void writeFromCSV(List<String> stringList,String filePath, boolean append){
+
+    public static void writeFromCSV(List<String> stringList, String filePath, boolean append) {
         File file = new File(filePath);
-        try (FileWriter fileWriter = new FileWriter(file,append);
-        BufferedWriter bufferedWriter = new BufferedWriter(fileWriter)
-        ){
-            for (String string:stringList){
+        FileWriter fileWriter = null;
+        BufferedWriter bufferedWriter = null;
+        try {
+            fileWriter = new FileWriter(file, append);
+            bufferedWriter = new BufferedWriter(fileWriter);
+            for (String string : stringList) {
                 bufferedWriter.write(string);
                 bufferedWriter.newLine();
             }
         } catch (IOException e) {
             throw new RuntimeException(e);
+        } finally {
+            try {
+                if (bufferedWriter != null) {
+                    bufferedWriter.close();
+                }
+                if (fileWriter != null) {
+                    fileWriter.close();
+                }
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+
         }
     }
 }
